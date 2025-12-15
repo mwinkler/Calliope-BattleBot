8a0785b10d19b15ba84065dbcf228c08864afae5
Matthias Winkler <mwinkler@gmx.ch>
2025-12-15 21:15:30 +0100

    Tweak values

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-enum RadioMessage {
-    message1 = 49434
-}
 function setMotorSpeed (motor: number, speed: number) {
     if (speed > 0) {
         maqueen.motorRun(motor, maqueen.Dir.CW, speed)
@@ -12,7 +9,7 @@ function setMotorSpeed (motor: number, speed: number) {
 }
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "b_1_on") {
-        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.InBackground)
+    	
     } else if (receivedString == "b_16_on") {
         maqueen.servoRun(maqueen.Servos.S1, 30)
     } else if (receivedString == "b_14_on") {
@@ -35,10 +32,10 @@ radio.onReceivedValue(function (name, value) {
 })
 function mapJoy (value: number, deadzone_low: number, deadzone_high: number, lowest: number, highest: number) {
     if (value > deadzone_high) {
-        return Math.map(value, deadzone_high, highest, 0, 255)
+        return Math.map(value, deadzone_high, highest, 0, 125)
     }
     if (value < deadzone_low) {
-        return Math.map(value, deadzone_low, lowest, 0, -255)
+        return Math.map(value, deadzone_low, lowest, 0, -125)
     }
     return 0
 }
@@ -49,7 +46,6 @@ lcd16x2rgb.setRGB1(lcd16x2rgb.lcd16x2rgb_eADDR(lcd16x2rgb.eADDR_RGB.RGB_16x2_V5)
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), true)
 lcd16x2rgb.clearScreen(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 maqueen.motorStop(maqueen.Motors.All)
-let max_speed = 25
 radio.setGroup(24)
 basic.forever(function () {
 	
