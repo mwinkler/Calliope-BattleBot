98763c65d530b0650c7ea341f393d0c7cf8d180b
Matthias Winkler <mwinkler@gmx.ch>
2025-12-20 21:48:22 +0100

    Add toggle for auto steering

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+function updateMotorSpeed () {
+    if (auto_steer_mode) {
+    	
+    } else {
+        setMotorSpeed(maqueen.Motors.M1, joy_y + joy_x)
+        setMotorSpeed(maqueen.Motors.M2, joy_y - joy_x)
+    }
+}
 function setMotorSpeed (motor: number, speed: number) {
     if (speed > 0) {
         maqueen.motorRun(motor, maqueen.Dir.CW, speed)
@@ -8,12 +16,12 @@ function setMotorSpeed (motor: number, speed: number) {
     }
 }
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "b_1_on") {
-    	
-    } else if (receivedString == "b_16_on") {
-        maqueen.servoRun(maqueen.Servos.S1, 30)
+    if (receivedString == "b_16_on") {
+        maqueen.servoRun(maqueen.Servos.S1, gripper_open ? 100 : 30)
+        gripper_open = !(gripper_open)
     } else if (receivedString == "b_14_on") {
-        maqueen.servoRun(maqueen.Servos.S1, 100)
+        auto_steer_mode = !(auto_steer_mode)
+        maqueen.setColor(auto_steer_mode ? 0xff8000 : 0x000000)
     }
 })
 radio.onReceivedValue(function (name, value) {
@@ -27,8 +35,7 @@ radio.onReceivedValue(function (name, value) {
         lcd16x2rgb.writeLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), "y: " + value + "   ")
         joy_y = mapJoy(value, 460, 510, 0, 980)
     }
-    setMotorSpeed(maqueen.Motors.M1, joy_y + joy_x)
-    setMotorSpeed(maqueen.Motors.M2, joy_y - joy_x)
+    updateMotorSpeed()
 })
 function mapJoy (value: number, deadzone_low: number, deadzone_high: number, lowest: number, highest: number) {
     if (value > deadzone_high) {
@@ -39,14 +46,24 @@ function mapJoy (value: number, deadzone_low: number, deadzone_high: number, low
     }
     return 0
 }
-let joy_y = 0
+let linesensor_r = false
+let linesensor_l = false
 let joy_x = 0
-lcd16x2rgb.initRGB(lcd16x2rgb.lcd16x2rgb_eADDR(lcd16x2rgb.eADDR_RGB.RGB_16x2_V5), true)
+let joy_y = 0
+let gripper_open = false
+let auto_steer_mode = false
+lcd16x2rgb.initRGB(lcd16x2rgb.lcd16x2rgb_eADDR(lcd16x2rgb.eADDR_RGB.RGB_16x2_V5), false)
 lcd16x2rgb.setRGB1(lcd16x2rgb.lcd16x2rgb_eADDR(lcd16x2rgb.eADDR_RGB.RGB_16x2_V5), 0xff00ff)
-lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), true)
+lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), false)
 lcd16x2rgb.clearScreen(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 maqueen.motorStop(maqueen.Motors.All)
+maqueen.servoRun(maqueen.Servos.S1, 100)
 radio.setGroup(24)
 basic.forever(function () {
-	
+    if (auto_steer_mode) {
+        linesensor_l = maqueen.readPatrol(maqueen.Patrol.PatrolLeft, maqueen.Brightness.Dark)
+        linesensor_r = maqueen.readPatrol(maqueen.Patrol.PatrolRight, maqueen.Brightness.Dark)
+        updateMotorSpeed()
+    }
+    basic.pause(100)
 })
